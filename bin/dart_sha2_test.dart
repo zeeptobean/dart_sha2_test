c5ddc832e6cdaf77d2edf8ad7ad73818a9c51ab3
zeeptobean <zeeptobean@gmail.com>
2026-01-31 21:21:41 +0700

    complete testing

--- bin/dart_sha2_test.dart
+++ bin/dart_sha2_test.dart
@@ -1,10 +1,12 @@
 import 'package:ansicolor/ansicolor.dart';
 import 'package:dart_sha2_test/core.dart';
-import 'package:dart_sha2_test/system_bench2.dart';
+import 'package:dart_sha2_test/system_bench.dart' as bench1;
+import 'package:dart_sha2_test/system_bench2.dart' as bench2;
 
 void main(List<String> arguments) async {
   ansiColorDisabled = false;
 
-  benchSha256(iterations: 50);
-  await benchSystemSha256(iterations: 50);
+  benchSha256(iterations: 100);
+  bench1.benchSystemSha256(iterations: 100);
+  await bench2.benchSystemSha256(iterations: 100);
 }
